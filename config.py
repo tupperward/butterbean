cc4466cd655cdfc79b96157224fbecf6084187c4
Ward <alexander.m.ward@gmail.com>
2019-09-09 01:36:30 -0400

    corrected typo

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from configparser import ConfigParser
 
-def config(filename='database.ini', section='postgressql'):
+def config(filename='database.ini', section='postgresql'):
     parser = ConfigParser()
     parser.read(filename)
 

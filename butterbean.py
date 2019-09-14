c64268902e971458fcbef2455c724b9a7619dbcd
Ward <alexander.m.ward@gmail.com>
2019-09-14 12:19:10 -0400

    aw fuck

--- butterbean.py
+++ butterbean.py
@@ -29,6 +29,28 @@ params = config()
 conn = psycopg2.connect(**params)
 cur = conn.cursor()
 
+greetMessage = "Welcome to the WATTBA-sistance! Please take your time to observe our rules and, if you're comfortable, use the **!callme** command to tag yourself with your pronouns. Available pronouns are **he/him**, **she/her**, **they/them**, **xe/xem** and **ze/zir** If you get tired of your pronouns you can remove them with **!imnot** \n\n There are several other roles you can **!join** too, like **Streampiggies** to be notified of Eli's streams. Check them out by using **!listroles**. \n\n Oh, and feel free to get an inspirational Bob Ross quote any time with **!bobross**. \n\n This server also uses this bot for meme purposes. Be on the lookout for memes you can send using by sending **!bb** and the name of the meme."
+
+def convertTuple(tup):
+    stringFormat = ''.join(tup)
+    return stringFormat
+
+def checkApprovedUsers(user):
+    cur.execute("SELECT * FROM approved_users;")
+    check = cur.fetchall()
+    results = []
+    for entry in check:
+        formatted = convertTuple(entry)
+        results.append(formatted)
+    if user in results:
+        return True
+    else:
+        return False
+
+def unapprovedDeny(user):
+    await ctx.send("Uh uh uh! " + user + " didn't say the magic word!")
+    await ctx.send("https://imgur.com/IiaYjzH")
+
 def closeSql():
     cur.close()
     conn.close()
@@ -45,32 +67,23 @@ async def bb(ctx, arg):
 #Mods can add items to the list
 @client.command()
 async def add(ctx, key, val):
-    check = None
-    cur.execute('SELECT %s FROM approved_users;'% ctx.message.author)
-    check = cur.fetchone()
-    if check:
+    if checkApprovedUsers(ctx.message.author):
         cur.execute("INSERT INTO posts VALUES ({0},{1});".format(key,val))
         conn.commit()
         await ctx.send("%s has been added to my necroborgic memories" % (key))
-
     else:
-        await ctx.send("Uh uh uh! " + ctx.message.author.mention + " didn't say the magic word!")
-        await ctx.send("https://imgur.com/IiaYjzH")
+        unapprovedDeny(ctx.message.author)
     closeSql()
 
 #Mods can remove items from the list
 @client.command()
 async def remove (ctx, key): 
-    check = None
-    cur.execute('SELECT %s FROM approved_users'% ctx.message.author)
-    check = cur.fetchone()
-    if check:
+    if checkApprovedUsers(ctx.message.author):
         cur.execute("DELETE FROM posts WHERE post_name = %s;"% (key))
         conn.commit()
         await ctx.send("%s has been purged from my necroborgic memories" % (key))
     else:
-        await ctx.send("Uh uh uh! " + ctx.message.author.mention + " didn't say the magic word!")
-        await ctx.send("https://imgur.com/IiaYjzH")
+        unapprovedDeny(ctx.message.author)
     closeSql()
 
 #Lists all meme commands
@@ -89,15 +102,16 @@ async def beanfo(ctx):
 async def on_member_join( member):
     guild = member.guild
     if guild.system_channel is not None:
-        eMessage = discord.Embed(description="{0.mention}! Welcome to the WATTBA-sistance! Please take your time to observe our rules and, if you're comfortable, use the **!callme** command to tag yourself with your pronouns. Available pronouns are **he/him**, **she/her**, **they/them**, **xe/xem** and **ze/zir** If you get tired of your pronouns you can remove them with **!imnot** \n\n There are several other roles you can **!join** too, like **Streampiggies** to be notified of Eli's streams. Check them out by using **!listroles**. \n\n Oh, and feel free to get an inspirational Bob Ross quote any time with **!bobross**. \n\n This server also uses this bot for meme purposes. Be on the lookout for memes you can send using by sending **!bb** and the name of the meme.".format(member))
+        eMessage = discord.Embed(description="{0.mention}! {1}".format(member, greetMessage))
         await guild.system_channel.send(embed=eMessage)
 
 
 #If needed, will resend the welcome message
 @client.command()
 async def resend(ctx):
-    eMessage = discord.Embed(description="Welcome to the WATTBA-sistance! Please take your time to observe our rules and, if you're comfortable, use the **!callme** command to tag yourself with your pronouns. Available pronouns are **he/him**, **she/her**, **they/them**, **xe/xem** and **ze/zir** If you get tired of your pronouns you can remove them with **!imnot** \n\n There are several other roles you can **!join** too, like **Streampiggies** to be notified of Eli's streams. Check them out by using **!listroles**. \n\n Oh, and feel free to get an inspirational Bob Ross quote any time with **!bobross**. \n\n This server also uses this bot for meme purposes. Be on the lookout for memes you can send using by sending **!bb** and the name of the meme.")
+    eMessage = discord.Embed(description="{0}".format(greetMessage))
     await ctx.send(embed=eMessage)
+
 #Bob Ross quote
 @client.command()
 async def bobross (ctx):
@@ -169,5 +183,4 @@ async def listroles(ctx):
     await ctx.send(rolesStr)
 
 #Actually running the damn thing
-
 client.run(os.environ['BOT_TOKEN'])

cf15a7569cdd269bba53346fbab5b6a11d8fd523
Ward <alexander.m.ward@gmail.com>
2020-01-02 21:13:34 -0500

    masking db creds

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 from configparser import ConfigParser
+import os
 
+"""
 def config(filename='database.ini', section='postgresql'):
+    
     parser = ConfigParser()
     parser.read(filename)
-
-    db = {}
+  
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
@@ -12,6 +14,17 @@ def config(filename='database.ini', section='postgresql'):
     else:
         raise Exception('Section {0} not found in the {1} file.'.format(section, filename))
     
+    return db
+"""
+
+def config():
+    db = {
+        'host': os.environ['PG_HOST'],
+        'database': os.environ['DATABASE'],
+        'user': os.environ['PG_USER'],
+        'password': os.environ['PG_PASS']
+    }
+
     return db
 
 def sanitize(arg):

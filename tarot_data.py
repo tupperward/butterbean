4a87be8d5e54a36a09a4009977256f43050e6209
Jinxed Feline <jinxedfeline@protonmail.com>
2020-10-18 01:45:05 +0200

    Add single card tarot draw functionality

---
+++ tarot_data.py
@@ -0,0 +1,12 @@
+tarotData = {'__template': 'This is just a template. What follows is some rough '
+              'info on the structure this data is supposed to look '
+              "like. 'deck' is an array of dicts.",
+'deck': [{'image': 'url for the card image',
+        'keywords': ['keyword 1', 'keyword 2, etc.'],
+        'meaning': 'a brief explanation of the card meaning',
+        'reading': '(currently unused) a helper string that mashes '
+                   'together the keywords with the card meaning',
+        'title': 'name of the card',
+        'url': 'url for a page explaining more detailed card meanings',
+        'yesno': "'yes', 'no' or 'maybe', depending on which answer "
+                 'this card represents to a yes/no question'}]}
